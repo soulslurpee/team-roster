4ee8b6c86539401d67746b87d3536c91eb2b5e03
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-10 14:25:28 -0400

    refactored Engineer, Intern, and Manger contructors to be children of Employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,10 +2,19 @@ const { TestWatcher } = require('jest');
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-  const employee = new Employee('Dave', '2', 'Engineer', 'dave@company.com');
+  const employee = new Employee('Dave', '2', 'employee', 'dave@company.com');
 
   expect(employee.name).toBe('Dave');
   expect(employee.id).toBe('2');
-  expect(employee.title).toBe('Engineer');
+  expect(employee.title).toBe('employee');
   expect(employee.email).toBe('dave@company.com');
+});
+
+test('creates bio array object', () => {
+
+  const employee = new Employee('Dave', '2', 'employee', 'dave@company.com', 'username');
+
+  expect(employee.getBio()).toEqual(expect.any(Object));
+
+  console.log(employee.getBio());
 });
