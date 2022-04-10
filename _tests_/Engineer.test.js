f4c4b5f2f972eb0015d9fb462c893978ea1fc99b
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-10 13:54:39 -0400

    refactored and simplified Engineer constructor, tests pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,6 +2,11 @@ const Engineer = require('../lib/Engineer.js');
 const { TestWatcher } = require('jest');
 
 test('checks for Engineer title', () =>{
-  const engineer = new Engineer('Dave', 'Engineer', 'dave@company.com');
-  expect(engineer.github);
+  const engineer = new Engineer('Dave', '2', 'Engineer', 'dave@company.com', 'username');
+
+  expect(engineer.name).toBe('Dave');
+  expect(engineer.id).toBe('2');
+  expect(engineer.title).toBe('Engineer');
+  expect(engineer.email).toBe('dave@company.com');
+  expect(engineer.special).toBe('username');
 });
